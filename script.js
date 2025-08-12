e53a7a0df60fdfcbe2b76b2b5404311a49f8ad6f
Mosh Hamedani <omoshola808@gmail.com>
2025-08-12 17:02:15 +0100

    inital commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// let person = {
+//   name: "mike",
+//   age: "23",
+//   isStudent: "true",
+//   address: {
+//     street: "ologolo",
+//     city: "lekki",
+//     state: "lagos",
+//   },
+//   hobbies: ["coding", "reading", "gaming"],
+// };
+
+// hobbies.push("eating");
+// console.log(hobbies);
+
+let name = ["ben", "sylas", "timo", "jola", "femi"];
+
+// let names = name.lastIndexOf("jola");
+
+console.log(name);
+
+// console.log(candidate1.lastIndexOf("jola"));
+
+// let numbers = "  h1  ";
+// let num1 = numbers.trim();
+
+// console.log(num1);
+
+let fruits = ["apple", "mango", "pineapple"];
+console.log(fruits.push("strawberry"));
